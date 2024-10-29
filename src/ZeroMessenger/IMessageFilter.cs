b5d972b92aa3181b75cbd2dbe6e5f36ff8e79e8d
AnnulusGames <annulusgames@gmail.com>
2024-10-29 10:56:53 +0900

    Rename: file

--- src/ZeroMessenger/IMessageFilter.cs
+++ src/ZeroMessenger/IMessageFilter.cs
@@ -9,5 +9,5 @@ public interface IMessageFilterBase
 [Preserve]
 public interface IMessageFilter<T> : IMessageFilterBase
 {
-    public abstract ValueTask InvokeAsync(T message, CancellationToken cancellationToken, Func<T, CancellationToken, ValueTask> next);
+    ValueTask InvokeAsync(T message, CancellationToken cancellationToken, Func<T, CancellationToken, ValueTask> next);
 }
